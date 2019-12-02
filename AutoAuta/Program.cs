b5f5b8cd091f7ba01657729e0cf8a260d910036c
Dolejš Daniel <dolejs.daniel.2017@skola.ssps.cz>
2019-12-02 16:16:45 +0100

    Přidejte soubory projektu.

---
+++ AutoAuta/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoAuta
+{
+    public enum Weather {Zataženo, Deštivo, Kalamita}
+    public enum TypTerenu {Rovina, Most, Tunel}
+    
+    public class WeatherGenerator
+    {
+        
+        public Weather Generuj(Random r)
+        {
+            Weather[] wArr = {Weather.Zataženo, Weather.Deštivo, Weather.Kalamita};
+            return wArr[r.Next(wArr.Length)];
+        }
+
+}
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
